1d979b1447a8ca7b50880ae659fee073695c932f
Manish-jadav <120002690+Manish-jadav@users.noreply.github.com>
2023-03-08 23:01:27 +0530

    js file

--- Script.js
+++ Script.js
@@ -1,62 +1,79 @@
-    document.addEventListener("DOMContentLoaded",function(){
-        createGrid(14);
+    document.addEventListener("DOMContentLoaded",function()
+    {
+        createGrid(12);
     });
-function createGrid(x) {
+
+function createGrid(x) 
+{
+
     var columns =x;
     var rows =x;
-
     var grid = document.createElement('div');
     grid.className = 'grid';
-    for (var i = 1; i <= columns; i++) {
+
+    for (var i = 1; i <= columns; i++) 
+    {
         var column = document.createElement('div'); // create column
         column.className = 'column';
         
-        for (var j = 1; j <= rows; j++) {
+        for (var j = 1; j <= rows; j++) 
+        {
             var row = document.createElement('div'); // create row
             row.className = 'row';
-            column.appendChild(row); // append row in column
-            
+            column.appendChild(row); // append row in column   
         }
-        grid.appendChild(column); // append column inside grid
-       
+        grid.appendChild(column); // append column inside grid  
     }
     document.body.appendChild(grid);
 
     $(".row").width(400/rows);
     $(".row").height(400/rows);
-    mycolor();
+
+    mycolor(); 
+    
 }    
+
 function mycolor()
 {
     var color=document.getElementById('favcolor').value;
-    
-            $(".row").mouseover(function() {
+
+        $(".row").mouseover(function() 
+        {
             $(this).css("background-color",color);
-    
-            });    
+        });    
 }
-function eraser(){
-    $(".row").mouseover(function() {
+
+function eraser()
+{
+    $(".row").mouseover(function() 
+    {
          $(this).css("background-color","");
-        });
+    });
 }
-function fun(){
+
+function fun()
+{
+    
     $(".grid").remove();
     
-    var columns =prompt("Enter Grid Width :");
-    var rows =prompt("Enter Grid Height :");
+    var columns = prompt("Enter Grid Height :");
+    var rows = prompt("Enter Grid Width :");
 
+    if(columns>=0 && rows>=0)
+    {
     var grid = document.createElement('div');
     grid.className = 'grid';
-    for (var i = 1; i <= columns; i++) {
+
+    for (var i = 1; i <= columns; i++) 
+    {
         var column = document.createElement('div'); // create column
         column.className = 'column';
         
-        for (var j = 1; j <= rows; j++) {
+        for (var j = 1; j <= rows; j++) 
+        {
             var row = document.createElement('div'); // create row
             row.className = 'row';
             column.appendChild(row); // append row in column
-            
         }
         grid.appendChild(column); // append column inside grid
     }
@@ -65,17 +82,14 @@ function fun(){
     $(".row").width(400/rows);
     $(".row").height(400/rows);
 
-    var color=document.getElementById('favcolor').value;
-  
-        $(".row").mouseover(function() {
-            $(this).css("background-color", color);
-        
-            });
-        
-        $(".row").click(function() {
-            $(this).css("background-color", "white");
-            });
-        }
+    mycolor();
+    }
+    else
+    {
+        alert("Enter Valid Height and Width !! ");
+        fun();
+    }
+}
 
         
     
